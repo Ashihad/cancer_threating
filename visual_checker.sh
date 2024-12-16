9f84172871d8a73891839effc207350b5429ca87
Ashihad <grzegorz.esser@gmail.com>
2024-12-16 01:31:18 +0100

    Updated visual checker for new data

--- visual_checker.sh
+++ visual_checker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
+echo cell: 1,0,0
 for x in $(seq 0 9); do
   for y in $(seq 0 9); do
     for z in $(seq 0 9); do
       echo
       echo searching for voxel $x $y $z
-      grep "^1,0,0,$x,$y,$z,*" output.csv
-      grep "^1,0,0,$x,$y,$z,*" cp-0_d3ddetector_voxel.csv
+      echo gen `grep -v '^-1,-1,-1,*' output.csv | grep "^1,0,0,$x,$y,$z,*" | cut -d ',' -f '-9,11-'`
+      echo src `grep "^1,0,0,$x,$y,$z,*" new/prostate_imrt_beam0_cp74_d3ddetector_voxel.csv`
     done
   done
 done
